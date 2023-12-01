819bfa210bcbeb9aa93a0f5361b863b261a3310e
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-12-01 01:14:23 +0100

    final update

--- src/components/Child2.tsx
+++ src/components/Child2.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../redux/store";
+import { useDispatch } from "react-redux";
 import { counterActions } from "../redux/slices/counter";
 import { productActions } from "../redux/slices/product";
 
 export default function Child2() {
-  const counterValue = useSelector((state: RootState) => state.counter.value);
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
   function increment() {
     dispatch(counterActions.increment());
   }
@@ -18,10 +16,11 @@ const dispatch = useDispatch();
     dispatch(productActions.changeName());
   }
 
-  return <div>
-    <button onClick={decrement}>-</button>
-    <button onClick={changeName}>changed name</button>
-
-  </div>;
-
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <button onClick={changeName}>changed name</button>
+    </div>
+  );
 }
